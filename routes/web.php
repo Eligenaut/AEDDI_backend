aac2f13dab61b980bef1a59ccaaae775cf11b8ab
Eligenaut <brunobrayane@gmail.com>
2025-05-11 02:01:24 +0300

    Modification de .env

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Connexion à la base de données réussie !";
+    } catch (\Exception $e) {
+        return "Erreur de connexion : " . $e->getMessage();
+    }
 });
