dad0bf91c290cb42d18b42457d3d7c912e8b21d0
Eligenaut <brunobrayane@gmail.com>
2025-05-11 16:02:18 +0300

    Message décrivant les changements

--- app/Models/Inscription.php
+++ app/Models/Inscription.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,7 +9,18 @@ class Inscription extends Model
 {
     use HasFactory;
 
+    // Indiquer les champs qui peuvent être remplis (mass assignment)
     protected $fillable = [
-        'nom', 'prenom', 'email', 'password', 'photo',
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'photo',
     ];
+
+    // Si tu utilises un timestamp personnalisé
+    public $timestamps = true;
+
+    // Si la table est différente (si tu ne respectes pas la convention de nommage)
+    protected $table = 'inscriptions';
 }
