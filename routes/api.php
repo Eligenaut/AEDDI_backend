4c505ce018520f3c189e928cf8bb38f5f33b9898
Eligenaut <brunobrayane@gmail.com>
2025-05-12 04:00:15 +0300

    Message décrivant les changements

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ConnexionController;
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'Frontend et Backend sont connectés !']);
 });
 Route::post('/inscription', [AuthController::class, 'register']);
+Route::post('/login', [ConnexionController::class, 'login']);
