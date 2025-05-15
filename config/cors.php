906cd53d04d5091536c9219896cd1b949bf392f9
Eligenaut <brunobrayane@gmail.com>
2025-05-15 14:20:59 +0300

    Modification du fichier FournisseurDetail.jsx et ProspectDetail.jsx

--- config/cors.php
+++ config/cors.php
@@ -1,30 +1,15 @@
 <?php
 
 return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-
-    'paths' => ['*'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://aeddi-antsiranana.onrender.com', 'http://localhost:*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-TOKEN', 'X-XSRF-TOKEN', 'Accept'],
 
     'exposed_headers' => [
         'Authorization',
@@ -35,10 +20,9 @@ return [
         'Accept'
     ],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
     'allowed_credentials' => true,
-
 ];
