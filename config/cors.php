c7e1f0d156e98b4733d1a62be7e205acfb969461
Eligenaut <brunobrayane@gmail.com>
2025-05-17 01:14:32 +0300

    Mise à jour Dockerfile, configuration Apache et ajout render.yaml

--- config/cors.php
+++ config/cors.php
@@ -2,14 +2,20 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'https://aeddi-antsiranana.onrender.com',
-        'http://localhost:3000',
     ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
-    'max_age' => 86400,
+
+    'max_age' => 0,
+
     'supports_credentials' => true,
 ];
